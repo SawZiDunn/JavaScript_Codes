fc823ed4cc8dfbf919592727d5cb4cf69bfe94bc
Saw Zi Dunn <szidunn199@gmail.com>
2023-09-01 23:10:14 +0630

    h

--- mongoDBfinding.js
+++ mongoDBfinding.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://helloworld123:mFH85XXSwL6Tk2o7@cluster1.hks96kj.mongodb.net/test";
+var url = "";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
