c1a405eece4981599170c14612d2a883e453bbda
SawZiDunn <szidunn199@gmail.com>
2023-12-10 18:27:36 +0630

    initial commit

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,9 +1,10 @@
-function binarySearch(array, tofind, start, end){
+function binarySearch(array, to_find, start, end){
     while(start <= end){
-        let median=Math.floor((start+end) / 2);
-        if(array[median] === tofind){
+        let median = Math.floor((start + end) / 2);
+
+        if(array[median] === to_find){
             return true;
-        }else if(array[median] < tofind){
+        }else if(array[median] < to_find){
             start = median + 1;
 
         }else {
@@ -12,11 +13,12 @@ function binarySearch(array, tofind, start, end){
     }
 }
 
-let myData=[2,7,9,56,89,92,93,94,95,96,97];
-let toFind=2;
+let myData = [2, 7, 9, 56, 89, 92, 93, 94, 95, 96, 97];
+let toFind = 2;
 
-if(binarySearch(myData,toFind,0,myData.length - 1)){
+if(binarySearch(myData, toFind, 0, myData.length - 1)){
     console.log("We found it.");
+
 }else{
     console.log("We haven't found.")
 }
